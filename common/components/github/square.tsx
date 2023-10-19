6660f80e6f7246ce8ff355e6f0f3fef2549d642d
FIQTOR <yttaufiq190@gmail.com>
2023-10-19 19:22:31 +0700

    1.3.37

--- common/components/github/square.tsx
+++ common/components/github/square.tsx
@@ -12,14 +12,14 @@ export default function Square({ data }: squareProps) {
             {data && data.user.contributionsCollection.contributionCalendar.weeks.slice(1).map((week: any, index: number) =>
                 <span key={index} className='w-full flex flex-col gap-px md:gap-1'>
                     {week.contributionDays.map((contributionDay: any, index_: number) =>
-                        <motion.span key={index_} className={`aspect-square bg-neutral-300 dark:bg-neutral-800`}
+                        <motion.div key={index_} className={`aspect-square bg-neutral-300 dark:bg-neutral-800`}
                             style={{
                                 backgroundColor: `${((contributionDay.contributionCount == 0) ? '' : contributionDay.color)}`
                             }}
                             initial={{ opacity: 0 }}
                             whileInView={{ opacity: 1 }}
                             transition={{ delay: (Math.random() * (0.5 - 0.1 + 0.5) + 0.1) }}
-                        />
+                        ></motion.div>
                     )}
                 </span>
             )}
