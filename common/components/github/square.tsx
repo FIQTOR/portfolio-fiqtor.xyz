d51374c57ea56e4c8b9fe4f7b8673974c999a912
FIQTOR <yttaufiq190@gmail.com>
2023-10-19 19:47:20 +0700

    1.3.40

--- common/components/github/square.tsx
+++ common/components/github/square.tsx
@@ -3,26 +3,19 @@ import { motion } from 'framer-motion';
 import React from 'react'
 
 interface squareProps {
-    data: any;
+    color: string;
+    key: number;
 }
 
-export default function Square({ data }: squareProps) {
+export default function Square({ color, key }: squareProps) {
     return (
-        <div className='flex gap-px md:gap-1'>
-            {data && data.user.contributionsCollection.contributionCalendar.weeks.slice(1).map((week: any, index: number) =>
-                <span key={index} className='w-full flex flex-col gap-px md:gap-1'>
-                    {week.contributionDays.map((contributionDay: any, index_: number) =>
-                        <motion.div key={index_} className={`aspect-square bg-neutral-300 dark:bg-neutral-800`}
-                            style={{
-                                backgroundColor: `${((contributionDay.contributionCount == 0) ? '' : contributionDay.color)}`
-                            }}
-                            initial={{ opacity: 0 }}
-                            whileInView={{ opacity: 1 }}
-                            transition={{ delay: (Math.random() * (0.5 - 0.1 + 0.5) + 0.1) }}
-                        ></motion.div>
-                    )}
-                </span>
-            )}
-        </div>
+        <motion.div key={key} className={`aspect-square bg-neutral-300 dark:bg-neutral-800`}
+            style={{
+                backgroundColor: `${color}`
+            }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: (Math.random() * (0.5 - 0.1 + 0.5) + 0.1) }}
+        ></motion.div>
     )
 }
